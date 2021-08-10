3120b09662d3f4bb008e38a23f220932bbacffd4
Qsymond <logvinov.evgeniy@ya.ru>
2021-08-11 00:15:48 +0300

    d02 ex00

--- src/com/school21/day02/ex00/Program.java
+++ src/com/school21/day02/ex00/Program.java
@@ -1,11 +1,7 @@
 package com.school21.day02.ex00;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -19,24 +15,57 @@ public class Program {
         while (scanner.hasNextLine())
         {
             String line = scanner.nextLine();
-            String extension = line.substring(0, line.indexOf(','));
+            String extension = line.substring(0, line.indexOf(',')).toUpperCase();
             String signature = line.substring(line.indexOf(',') + 2);
             signaturesMap.put(extension, signature);
         }
         return signaturesMap;
     }
 
-    public static void main(String[] args) throws IOException {
-        File file = new File("/Users/qsymond/Desktop/files/file1.pdf");
+    public static String byteToHex(byte num) {
+        char[] hexDigits = new char[2];
+        hexDigits[0] = Character.forDigit((num >> 4) & 0xF, 16);
+        hexDigits[1] = Character.forDigit((num & 0xF), 16);
+        return new String(hexDigits);
+    }
+
+    public static void findSignature(String fileName) throws IOException {
+        File file = new File(fileName);
         FileInputStream fileInputStream = new FileInputStream(file);
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < 12; i++)
-            stringBuilder.append(Integer.toHexString(fileInputStream.read())).append(" ");
+            stringBuilder.append(byteToHex((byte) fileInputStream.read())).append(" ");
         fileInputStream.close();
-
+        Writer writer = new OutputStreamWriter(new FileOutputStream(RESULT_FILE, true));
         Map<String, String> signatures = getSignatures(SIG_FILE);
+        boolean isfound = false;
         for (Map.Entry entry : signatures.entrySet())
         {
+            if (stringBuilder.toString().toUpperCase().startsWith(entry.getValue().toString()))
+            {
+                writer.write(entry.getKey().toString() + "\n");
+                System.out.println("PROCESSED");
+                isfound = true;
+                break;
+            }
+        }
+        if (!isfound)
+            System.out.println("UNDEFINED");
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String line = "";
+        while (!line.equals("42")) {
+            System.out.print("-> ");
+            line = scanner.nextLine();
+            try {
+                findSignature(line);
+            } catch (Exception e)
+            {
+                System.out.println("Некорректное имя файла!");
+            }
 
         }
     }
