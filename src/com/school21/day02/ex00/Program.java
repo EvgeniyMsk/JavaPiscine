5c034add00f94bb6ff3ee5d9e36041ecc9508c65
Qsymond <logvinov.evgeniy@ya.ru>
2021-08-28 01:10:42 +0300

    d02-100

--- src/com/school21/day02/ex00/Program.java
+++ src/com/school21/day02/ex00/Program.java
@@ -1,72 +1,80 @@
 package com.school21.day02.ex00;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Program {
-    private static final String SIG_FILE = "/Users/qsymond/Desktop/JavaPiscine/src/com/school21/day02/ex00/signatures.txt";
-    private static final String RESULT_FILE = "result.txt";
+    private static Map<String, String> signatures;
+    private static final String SIGFILE = "signatures.txt";
+    private static final String RESULTFILE = "result.txt";
 
-    private static Map<String, String> getSignatures(String file) throws FileNotFoundException {
-        Map<String, String> signaturesMap = new HashMap<>();
-        Scanner scanner = new Scanner(new File(file));
+    private static String byteToHex(byte number) {
+        char[] digits = new char[2];
+        digits[0] = Character.forDigit((number >> 4) & 0xF, 16);
+        digits[1] = Character.forDigit(number & 0xF, 16);
+        return new String(digits);
+    }
+
+    private static Map<String, String> initSignatures() throws FileNotFoundException {
+        File file = new File(SIGFILE);
+        Scanner scanner = new Scanner(file);
+        Map<String, String> result = new HashMap<>();
         while (scanner.hasNextLine())
         {
             String line = scanner.nextLine();
-            String extension = line.substring(0, line.indexOf(',')).toUpperCase();
-            String signature = line.substring(line.indexOf(',') + 2);
-            signaturesMap.put(extension, signature);
+            String key = line.substring(0, line.indexOf(" ") - 1);
+            String value = line.substring(line.indexOf(" ") + 1);
+            result.put(key, value);
         }
-        return signaturesMap;
+        scanner.close();
+        return result;
     }
 
-    public static String byteToHex(byte num) {
-        char[] hexDigits = new char[2];
-        hexDigits[0] = Character.forDigit((num >> 4) & 0xF, 16);
-        hexDigits[1] = Character.forDigit((num & 0xF), 16);
-        return new String(hexDigits);
+    private static String isInSignatures(String sign) {
+        for (Map.Entry<String, String> pair : signatures.entrySet())
+            if (pair.getValue().equalsIgnoreCase(sign.substring(0, sign.length() - 1)))
+                return pair.getKey();
+        return null;
     }
 
-    public static void findSignature(String fileName) throws IOException {
-        File file = new File(fileName);
-        FileInputStream fileInputStream = new FileInputStream(file);
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < 12; i++)
-            stringBuilder.append(byteToHex((byte) fileInputStream.read())).append(" ");
-        fileInputStream.close();
-        Writer writer = new OutputStreamWriter(new FileOutputStream(RESULT_FILE, true));
-        Map<String, String> signatures = getSignatures(SIG_FILE);
-        boolean isfound = false;
-        for (Map.Entry entry : signatures.entrySet())
-        {
-            if (stringBuilder.toString().toUpperCase().startsWith(entry.getValue().toString()))
-            {
-                writer.write(entry.getKey().toString() + "\n");
-                System.out.println("PROCESSED");
-                isfound = true;
-                break;
-            }
-        }
-        if (!isfound)
-            System.out.println("UNDEFINED");
-        writer.close();
-    }
-
-    public static void main(String[] args) throws IOException {
+    private static void doExercize() throws IOException {
         Scanner scanner = new Scanner(System.in);
-        String line = "";
-        while (!line.equals("42")) {
-            System.out.print("-> ");
-            line = scanner.nextLine();
+        String fileName;
+        while (true) {
+            fileName = scanner.nextLine();
+            if (fileName.equals("42"))
+            {
+                scanner.close();
+                return;
+            }
             try {
-                findSignature(line);
-            } catch (Exception e)
+                String sign = "";
+                FileInputStream fileInputStream = new FileInputStream(fileName);
+                for (int i = 0; i < 8; i++)
+                {
+                    sign = sign + byteToHex((byte) fileInputStream.read()) + " ";
+                    if (isInSignatures(sign) != null)
+                        break;
+                }
+                fileInputStream.close();
+                if (isInSignatures(sign) != null)
+                {
+                    System.out.println("PROCESSED");
+                    Writer writer = new OutputStreamWriter(new FileOutputStream(RESULTFILE, true));
+                    writer.write(isInSignatures(sign) + "\n");
+                    writer.close();
+                }
+                else
+                    System.out.println("UNDEFINED");
+            } catch (FileNotFoundException e)
             {
                 System.out.println("Некорректное имя файла!");
             }
-
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        signatures = initSignatures();
+        doExercize();
     }
 }
